c769a3570513e8af3abdc0cb58d82a66397bc1c1
Ronald Regalado Batista <112780292+Ronald-Regalado@users.noreply.github.com>
2023-09-21 11:41:23 -0400

    Update languages.js

--- src/languages.js
+++ src/languages.js
@@ -8,7 +8,11 @@ const userLang=navigator.language.substring(0, 2);
 
 //Get json that correspond to language needed
 const changeLanguage = async language => {
-    const requestJson = await fetch(`./language/${language}.json`);
+    const requestJson = await fetch(`./language/${language}.json`, {
+      headers : { 
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+       });
     const texts = await requestJson.json();
     for (const textToChange of textsToChange) {
         const section = textToChange.dataset.section;
